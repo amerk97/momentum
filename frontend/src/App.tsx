e9c71c0081b1cf178961e0c1919ae398332d314f
amerk97 <amerkalac@hotmail.com>
2025-03-14 11:46:16 +0100

    Add Tailwind 3 CSS configuration and integrate into the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,18 @@ import './App.css'
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
+  
   const [count, setCount] = useState(0)
 
   return (
+  <>
+   <div className="bg-blue-500 text-white p-4 text-center text-xl font-bold">
+   Tailwind v3 is working
+   </div>
+
    <AppRoutes />
-  )
+  </>
+  );
 }
 
 export default App
